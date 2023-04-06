ed44e05979b280d73aeab6c13e139e8cb411a648
Kaisoku <khuss063@gmail.com>
2023-04-06 17:10:09 +0200

    dev: data binding

--- angular-starter/src/app/servers/servers.component.ts
+++ angular-starter/src/app/servers/servers.component.ts
@@ -4,8 +4,25 @@ import { Component } from '@angular/core';
   //selector: '[app-servers]', attribute selector
   //selector: '.app-servers', class selector
   selector: 'app-servers',
-  //templateUrl: './servers.component.html',
-  template: ` <app-server></app-server><app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  //template: ` <app-server></app-server><app-server></app-server>`,
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {}
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
+
+  constructor() {
+    setTimeout(() => (this.allowNewServer = true), 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServer(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
