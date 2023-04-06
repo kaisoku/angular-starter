87c2318d194e673dbf7a694f72448174355e8db3
Kaisoku <khuss063@gmail.com>
2023-04-06 19:17:32 +0200

    dev: directive

--- angular-starter/src/app/servers/servers.component.ts
+++ angular-starter/src/app/servers/servers.component.ts
@@ -12,12 +12,16 @@ export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'TestServer';
+  serverCreated = false;
+  servers = ['TestServer', 'TestServer2'];
 
   constructor() {
     setTimeout(() => (this.allowNewServer = true), 2000);
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus =
       'Server was created! Name is ' + this.serverName;
   }
